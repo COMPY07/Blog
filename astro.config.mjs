419537446f6a137187118fc9d7417b9b2ceb567b
COMPY07 <dlwjdgns13579@gmail.com>
2024-06-22 11:39:04 -0700

    new_post

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://COMPY07.github.io/",
-  base: "/",
+  base: "Blog/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
